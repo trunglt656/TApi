c0d3eb85dc542ec75b186f738b202a3c8e752e8a
Trung Tran <20050014@student.bdu.edu.vn>
2023-08-07 22:54:27 +0700

    T

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import "package:http/http.dart" as http;
 
@@ -9,19 +11,34 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  List<dynamic> users = [];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: Text("REst API"),
         ),
+        body: ListView.builder(itemBuilder: (context, index) {
+          final user = users[index];
+          final email = user['email'];
+          return ListTile(title: Text(email));
+        }),
         floatingActionButton: FloatingActionButton(
-          onPressed: CallName,
+          onPressed: fetchUsers,
           child: Icon(Icons.add),
         ));
   }
 
-  void CallName() {
-    print("Hello World");
+  void fetchUsers() async {
+    print("fetching users called");
+    const url = "https://randomuser.me/api/?results=100";
+    final uri = Uri.parse(url);
+    final response = await http.get(uri);
+    final body = response.body;
+    final json = jsonDecode(body);
+    setState(() {
+      users = json['results'];
+    });
+    print('fetch Users called');
   }
 }
